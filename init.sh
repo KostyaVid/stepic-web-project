c7334e9b62378fdb82d0f87469335b781da41004
KostyaVid <constat@live.com>
2022-11-17 21:20:56 +0300

    add django

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo gunicorn --bind='0.0.0.0:8080' hello:app
+cd web
+sudo -c gunicorn --bind='0.0.0.0:8080' hello:app
+sudo -c gunicorn --bind='0.0.0.0:8000' ask.wsgi:application
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
