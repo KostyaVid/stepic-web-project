211257a6dbe26c96647c5a8e90180bd9e8900a6a
KostyaVid <constat@live.com>
2022-11-17 18:49:54 +0300

    fix_final

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def app(env, start_response):  
-   body = '/n'.join(env['QUERY_STRING'].split('&')).encode('utf-8')
+   body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
    headers = [('Content-type', 'text/plain; charset=utf-8')] 
    start_response('200 OK', headers)
-   return  [body]
+   return  body
