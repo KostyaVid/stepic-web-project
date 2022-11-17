19048a583c2d6f620ca876e5acdc79211388d042
KostyaVid <constat@live.com>
2022-11-17 18:10:08 +0300

    s

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def app(env, start_response):  
    body = '/n'.join(env['QUERY_STRING'].split('&'))
-   headers = [('Content-type', 'text/plain'),('Content-length',bytes(str(len(body))))] 
+   headers = [('Content-type', 'text/plain'),('Content-Length',bytes(str(len(body))))] 
    start_response('200 OK', headers)
-   return  body
+   return  [body]
